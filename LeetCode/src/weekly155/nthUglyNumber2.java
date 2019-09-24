bcc108ba5f6807d3c11215e339ada7b18a886134
Frank <1005499082@qq.com>
2019-09-24 20:58:20 +0800

    it is very important to be long enough

--- LeetCode/src/weekly155/nthUglyNumber2.java
+++ LeetCode/src/weekly155/nthUglyNumber2.java
@@ -1,28 +1,30 @@
 package weekly155;
 
 public class nthUglyNumber2 {
-	public int gcd(int a, int b){
+	public long gcd(long a, long b){
 		return b == 0 ? a : gcd(b, a % b);
 	}
 	
-	public int lcm(int a, int b){
+	public long lcm(long a, long b){
 		return a * b / gcd(a, b);
 	}
 	
-	public int number(int a, int b, int c, int tar){
+	public long number(int a, int b, int c, int tar){
+		System.out.println(lcm(b,c) + "-" + lcm(a,b) + "-"+lcm(a,c)+"-"+lcm(a,lcm(b,c)));
 		return tar / a + tar / b + tar / c - tar / lcm(a, b)
 			   - tar/lcm(b,c) - tar/lcm(a,c) + tar/lcm(a, lcm(b, c));
 	}
 	
-	public void te(int a,int b, int c, int n){
-		System.out.println(number(a, b, c,1999999981));
-	}
+//	public void te(int a,int b, int c, int n){
+//		System.out.println(number(a, b, c,1999999978));
+//	}
 	
 	public int nthUglyNumber(int n, int a, int b, int c) {
 		int min = 1;
-		int max = Integer.MAX_VALUE;
+		int max = 2000000000;
 		while(min < max){
 			int mid = min + (max - min) / 2;
+			System.out.println("mid:"+mid+"cot:"+number(a,b,c,mid));
 			if(number(a, b, c, mid) < n)//很巧妙地从小到大往上二分
 				min = mid + 1;
 			else
--- LeetCode/src/weekly155/test.java
+++ LeetCode/src/weekly155/test.java
@@ -8,12 +8,19 @@ public class test {
 			System.out.println(m.minimumAbsDifference(new int[]{3,8,-10,23,19,-4,-14,27}));
 		*/
 			//System.out.println(1 % 1);
-//			nthUglyNumber i = new nthUglyNumber();
-//			System.out.println(i.nthUglyNumber(6, 2, 1, 3));
+//			1000000000
+//			2
+//			217983653
+//			336916467
+			nthUglyNumber2 i = new nthUglyNumber2();
+			//i.te(2, 217983653, 336916467, 1999999978);
+			System.out.println(i.nthUglyNumber(1000000000,2,217983653,336916467));
+			//System.out.println(i.nthUglyNumber(4, 2, 3, 4));
 			//System.out.println('a' < 'b');
-			List<Integer> l = new ArrayList<>();
-			l.add(2);
-			System.out.println(l.size());
-			System.out.println(l.get(0));
+//			List<Integer> l = new ArrayList<>();
+//			l.add(2);
+//			System.out.println(l.size());
+//			System.out.println(l.get(0));
+			
 		}
 }
